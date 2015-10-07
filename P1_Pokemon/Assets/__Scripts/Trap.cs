1d4ed0d6679467820360aeec06414358f4702c55
Agam Batra <agambatra8@gmail.com>
2015-10-07 07:11:43 -0400

    added in a trap prefab

---
+++ P1_Pokemon/Assets/__Scripts/Trap.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trap : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider coll){
+		//set back who ever collided
+		//delete the trap
+	}
+}
